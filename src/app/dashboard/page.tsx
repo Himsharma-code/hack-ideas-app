0544198789db93f79ee3ad845b080bc931f69c0e
Himsharma-code <himanshu15101998@gmail.com>
2024-01-13 18:49:48 +0530

    fix: api testing

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,28 +1,27 @@
 "use client";
 import DashBoard from "@/pages/DashBoard";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const DashBoardPage = () => {
-  // const [tasks, setTasks] = useState([]);
-  // useEffect(() => {
-  //   // Fetch data from the API when the component mounts
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch("/api/users/getAllUserTasks");
-  //       const data = await response.json();
+  useEffect(() => {
+    // Fetch data from the API when the component mounts
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/users/getAllUserTasks");
+        const data = await response.json();
 
-  //       if (data.success) {
-  //         setTasks(data.allTasks);
-  //       } else {
-  //         console.error(data.error);
-  //       }
-  //     } catch (error) {
-  //       console.error("Error fetching data:", error);
-  //     }
-  //   };
+        if (data.success) {
+          // setTasks(data.allTasks);
+        } else {
+          console.error(data.error);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-  //   fetchData();
-  // }, []);
+    fetchData();
+  }, []);
   return <DashBoard />;
 };
 
